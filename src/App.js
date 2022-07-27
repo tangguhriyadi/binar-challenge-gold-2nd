e6e371fcb087993f6469778ca14c7b1762421a31
tangguhriyadi <mtangguh97@gmail.com>
2022-07-27 11:59:43 +0700

    install dependencies

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
 
 function App() {
   return (
