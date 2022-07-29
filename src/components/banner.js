602662958d51f6489a4acbeaccaccb1696c520d9
tangguhriyadi <mtangguh97@gmail.com>
2022-07-29 12:57:42 +0700

    back button

--- src/components/banner.js
+++ src/components/banner.js
@@ -11,7 +11,7 @@ const Banner = ({showButton}) => {
             <Col className="bannerLeft" style={{paddingLeft:'136px', paddingTop:'118px', paddingRight:'16px'}} md={6}>
                 <h1 className='font1'>Sewa & Rental Mobil Terbaik di kawasan (Lokasimu)</h1>
                 <div className='font2' style={{marginTop:'16px', marginBottom:'16px'}}>Selamat datang di Binar Car Rental. Kami menyediakan mobil kualitas terbaik dengan harga terjangkau. Selalu siap melayani kebutuhanmu untuk sewa mobil selama 24 jam.</div>
-                {showButton ? (<Button as={Link} to="/search" className='btn1' style={{marginTop:'16px'}}>Mulai Sewa Mobil</Button>) : <></>}
+                {showButton ? (<Button as={Link} to="/search" className='btn1' style={{marginTop:'16px'}}>Mulai Sewa Mobil</Button>) : (<Button as={Link} to="/" className='btn1' style={{marginTop:'16px'}}>Kembali</Button>)}
                 
                 
             </Col>
